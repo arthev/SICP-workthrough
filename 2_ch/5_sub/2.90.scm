7d8eed378aedd725e0592e16eb897b99cc86c53c
Arthur Evensen <evensen.arthur.n@gmail.com>
2018-01-05 18:40:51 +0100

    'Finish' chapter 2 (but saving the last extended exercise for after chapter three since it calls for the system to actually be tested and used.)

--- 2_ch/5_sub/2.90.scm
+++ 2_ch/5_sub/2.90.scm
@@ -1,15 +1,16 @@
 (define (negate x) (apply-generic 'negate x))
+(define (empty-termlist? term-list) (apply-generic 'empty-termlist? term-list))
+(define (first-term term-list) (apply-generic 'first-term term-list))
+(define (rest-terms term-list) (apply-generic 'rest-terms term-list))
+(define (the-empty-termlist) ((get 'the-empty-termlist 'sparse)))
+(define (make-term order coeff) ((get 'make-term 'sparse) order coeff))
+...
+;Slight hack of just grabbing one when there's really no data to actually go by, because it's the initial step, so to say.
+;The elegant solution would be to modify the polynomial package itself so it elects the type to lookup for those oddball cases
+;depending on, say, the x of the (x . y) polynomials it's mathing away on.
 
-(define (install-polynomial-package)
-  ;;Internal procedures
-  ;;Representation of poly
-  (define (make-poly variable term-list) (cons variable term-list))
-  (define (variable p) (car p))
-  (define (term-list p) (cdr p))
-  (define (variable? x) (symbol? x))
-  (define (same-variable? v1 v2)
-	(and (variable? v1) (variable? v2) (eq? v1 v2)))
-  ;;Representation of terms and term lists
+(define (install-dense-package)
+  ;;Internal procedures for representation of data
   (define (adjoin-term term term-list)
 	(cond ((=zero? (coeff term)) term-list)
 		  ((= (- (order term) (order (first-term term-list))) 1)
@@ -24,7 +25,74 @@
   (define (make-term order coeff) (list order coeff)) 
   (define (order term) (car term)) 
   (define (coeff term) (cadr term)) 
-  (define (add-poly p1 p2)
+  ;;Interface to the rest of the system
+  (define (tag x) (attach-tag 'dense x))
+  (put 'adjoin-term 'dense
+	   (lambda (term term-list) (tag (adjoin-term term term-list))))
+  (put 'the-empty-termlist 'dense
+	   (tag (the-empty-termlist)))
+  (put 'first-term 'dense
+	   (lambda (term-list) (tag (first-term term-list))))
+  (put 'rest-terms 'dense
+	   (lambda (term-list) (tag (rest-terms term-list))))
+  (put 'empty-termlist? 'dense
+	   empty-termlist?)
+  (put 'make-term 'dense
+	   make-term)
+  (put 'order 'dense
+	   order)
+  (put 'coeff 'dense
+	   coeff)
+  'done)
+(define adjoin-term-dense (get 'adjoin-term 'dense))
+(define make-term-dense (get 'make-term 'dense))
+;Because the dense and sparse package really share term-representation, the optimal solution might actually be a separate term package...
+
+
+(define (install-sparse-package)
+  (define (adjoin-term term term-list)
+	(if (=zero? (coeff term))
+	  term-list
+	  (cons term term-list)))
+  (define (the-empty-termlist) '())
+  (define (first-term term-list (car term-list)))
+  (define (rest-terms term-list (cdr term-list)))
+  (define (empty-termlist? term-list) (null? term-list))
+  (define (make-term order coeff) (list order coeff))
+  (define (order term) (car term))
+  (define (coeff term) (cadr term))
+  ;;Interface
+  (define (tag x) (attach-tag 'sparse x))
+  (put 'adjoin-term 'sparse
+	   (lambda (term term-list) (tag (adjoin-term term term-list))))
+  (put 'the-empty-termlist 'sparse
+	   (tag (the-empty-termlist)))
+  (put 'first-term 'sparse
+	   first-term)
+  (put 'rest-terms 'sparse
+	   (lambda (term-list) (tag (rest-terms term-list))))
+  (put 'empty-termlist? 'sparse
+	   empty-termlist?)
+  (put 'make-term 'sparse
+	   make-term)
+  (put 'order 'sparse
+	   order)
+  (put 'coeff 'sparse
+	   coeff)
+  'done)
+
+
+(define (install-polynomial-package)
+  ;;Internal procedures
+  ;;Representation of poly
+  (define (make-poly variable term-list) (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  (define (variable? x) (symbol? x))
+  (define (same-variable? v1 v2)
+	(and (variable? v1) (variable? v2) (eq? v1 v2)))
+  ;;Representation of terms and term lists
+    (define (add-poly p1 p2)
 	(if (same-variable? (variable p1) (variable p2))
 	  (make-poly (variable p1)
 				 (add-terms (term-list p1) (term-list p2)))
